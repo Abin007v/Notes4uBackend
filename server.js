588b6be03bfa95235b69501a59b0d613e9e195cd
Abin v <abingithub@gmail.com>
2022-03-30 15:11:33 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
+app.use(notFound, errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`port started on ${PORT}`));
